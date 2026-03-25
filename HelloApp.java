9b50cabb24a63c3009f7d25b8572573a5f832205
Ayush6818 <ayush.wwekumar2633@gmail.com>
2026-03-26 01:05:07 +0530

    UC6 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,24 +1,20 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
 
-            StringBuilder names = new StringBuilder();
-            boolean first = true;
+            StringBuilder nameBuilder = new StringBuilder();
 
-            
             for (String name : args) {
-                if (!first) {
-                    names.append(", ");
-                }
-                names.append(name);
-                first = false;
+                nameBuilder.append(name).append(", ");
             }
 
-            System.out.println("Hello, " + names.toString() + "!");
+            
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
